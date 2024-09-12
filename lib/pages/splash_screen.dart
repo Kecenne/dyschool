45ae85153e9763fe512c098f7e3000cf138ad662
julienNtb <julien.nottebaert1@gmail.com>
2024-09-12 14:42:54 +0200

    changement timing splash screen

--- lib/pages/splash_screen.dart
+++ lib/pages/splash_screen.dart
@@ -13,7 +13,7 @@ class _SplashScreenState extends State<SplashScreen> {
     super.initState();
 
     // Démarre un Timer qui attend 2,2 secondes avant de rediriger
-    Timer(Duration(seconds: 2, milliseconds: 200), () {
+    Timer(Duration(seconds: 1, milliseconds: 900), () {
       // Redirection vers la HomePage après 2,2 secondes
       Navigator.of(context).pushReplacement(
         MaterialPageRoute(builder: (context) => HomePage()),
