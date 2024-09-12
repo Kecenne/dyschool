781240d1e61676c6cfef1e1bc674247da3ecbeb8
julienNtb <julien.nottebaert1@gmail.com>
2024-09-12 12:22:47 +0200

    ajout de l'image du splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:async';  // Pour utiliser Timer
 import 'package:flutter/material.dart';
 import 'pages/home_page.dart';
 import 'pages/progression_page.dart';
@@ -20,11 +21,48 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MainPage(),
+      home: const SplashScreen(), // Lancer le splash screen en premier
     );
   }
 }
 
+// SplashScreen Widget
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    // Timer pour attendre 2,2 secondes avant de rediriger vers MainPage
+    Timer(const Duration(seconds: 2, milliseconds: 200), () {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (context) => const MainPage()), // Rediriger vers MainPage
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white, // Couleur d'arrière-plan
+      body: Center(
+        child: Image.asset(
+          'assets/images/logo.png', // Remplace par le chemin de ton logo
+          width: 150,
+          height: 150,
+        ),
+      ),
+    );
+  }
+}
+
+// MainPage Widget inchangé
 class MainPage extends StatefulWidget {
   const MainPage({Key? key}) : super(key: key);
 
@@ -90,7 +128,7 @@ class _MainPageState extends State<MainPage> {
           currentIndex: _selectedIndex,
           selectedItemColor: Colors.deepPurple,
           unselectedItemColor: Colors.grey,
-          backgroundColor:  Colors.white,
+          backgroundColor: Colors.white,
           selectedFontSize: 20,
           unselectedFontSize: 20,
           onTap: _onItemTapped,
---
+++ lib/pages/splash_screen.dart
@@ -0,0 +1,37 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'home_page.dart'; // Importer la page d'accueil
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    // Démarre un Timer qui attend 2,2 secondes avant de rediriger
+    Timer(Duration(seconds: 2, milliseconds: 200), () {
+      // Redirection vers la HomePage après 2,2 secondes
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (context) => HomePage()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white, // Choisis la couleur d'arrière-plan de ta splash screen
+      body: Center(
+        child: Image.asset(
+          'assets/images/logo.png', // Chemin vers le logo
+          width: 150, // Largeur du logo
+          height: 150, // Hauteur du logo
+        ),
+      ),
+    );
+  }
+}
