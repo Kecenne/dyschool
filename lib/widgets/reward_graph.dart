aeeab74a64e2f830fa1bfae77849dff75cea531b
Kecenne <alexisbouffard1007@gmail.com>
2025-02-20 16:27:08 +0100

    changed reward graph

--- lib/widgets/reward_graph.dart
+++ lib/widgets/reward_graph.dart
@@ -1,9 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import '../services/medal_manager.dart';
 import 'package:intl/intl.dart';
 import 'package:intl/date_symbol_data_local.dart';
+import '../services/medal_manager.dart';
 
 class RewardGraph extends StatefulWidget {
   const RewardGraph({Key? key}) : super(key: key);
@@ -15,7 +16,9 @@ class RewardGraph extends StatefulWidget {
 class _RewardGraphState extends State<RewardGraph> {
   DateTime? firstMedalDate;
   DateTime selectedMonth = DateTime.now();
-  bool showAll = false; 
+  bool showAll = false;
+  int touchedGroupIndex = -1;
+  int rotationTurns = 1;
 
   @override
   void initState() {
@@ -72,7 +75,7 @@ class _RewardGraphState extends State<RewardGraph> {
   @override
   Widget build(BuildContext context) {
     final medalManager = Provider.of<MedalManager>(context);
-    
+
     return FutureBuilder<Map<String, int>>(
       future: showAll ? medalManager.getTotalMedals() : medalManager.getMedalsByMonth(selectedMonth),
       builder: (context, snapshot) {
@@ -81,12 +84,7 @@ class _RewardGraphState extends State<RewardGraph> {
         }
 
         final medals = snapshot.data!;
-        int totalMedals = medals['gold']! + medals['silver']! + medals['bronze']!;
-        double graphHeight = 250;
-
-        double goldHeight = (medals['gold']! / 90) * graphHeight;
-        double silverHeight = (medals['silver']! / 90) * graphHeight;
-        double bronzeHeight = (medals['bronze']! / 90) * graphHeight;
+        int maxMedals = 90;
 
         return Column(
           crossAxisAlignment: CrossAxisAlignment.start,
@@ -97,6 +95,7 @@ class _RewardGraphState extends State<RewardGraph> {
             ),
             const SizedBox(height: 16),
 
+            // Sélecteur de mois / Tous
             Container(
               decoration: BoxDecoration(
                 color: const Color(0xFF6B9DA4),
@@ -112,7 +111,6 @@ class _RewardGraphState extends State<RewardGraph> {
                         ? () => _changeMonth(-1)
                         : null,
                   ),
-
                   Column(
                     children: [
                       Text(
@@ -122,12 +120,11 @@ class _RewardGraphState extends State<RewardGraph> {
                         style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.white),
                       ),
                       Text(
-                        "$totalMedals hérissons",
+                        "${medals['gold']! + medals['silver']! + medals['bronze']!} hérissons",
                         style: const TextStyle(fontSize: 14, color: Colors.white70),
                       ),
                     ],
                   ),
-
                   IconButton(
                     icon: const Icon(Icons.arrow_right, size: 28, color: Colors.white),
                     onPressed: showAll ? null : () => _changeMonth(1),
@@ -137,41 +134,74 @@ class _RewardGraphState extends State<RewardGraph> {
             ),
             const SizedBox(height: 24),
 
-            Container(
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(16),
-              ),
-              padding: const EdgeInsets.all(16),
-              width: double.infinity,
-              height: 300,
-              child: Stack(
-                children: [
-                  Positioned.fill(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        _buildGraphLine("90"),
-                        _buildGraphLine("60"),
-                        _buildGraphLine("30"),
-                        _buildGraphLine("0"),
-                      ],
+            AspectRatio(
+              aspectRatio: 2.5,
+              child: BarChart(
+                BarChartData(
+                  alignment: BarChartAlignment.spaceBetween,
+                  maxY: 90,
+                  borderData: FlBorderData(
+                    show: true,
+                    border: const Border.symmetric(
+                      horizontal: BorderSide(color: Colors.grey, width: 1),
                     ),
                   ),
-
-                  Align(
-                    alignment: Alignment.bottomLeft,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        _buildMedalColumn("assets/images/rewards/Bronze.png", bronzeHeight, medals['bronze']!),
-                        _buildMedalColumn("assets/images/rewards/Silver.png", silverHeight, medals['silver']!),
-                        _buildMedalColumn("assets/images/rewards/Gold.png", goldHeight, medals['gold']!),
-                      ],
+                  gridData: FlGridData(
+                    show: true,
+                    drawVerticalLine: false,
+                    getDrawingHorizontalLine: (value) => FlLine(
+                      color: Colors.grey,
+                      strokeWidth: 1,
                     ),
                   ),
-
-                ],
+                  titlesData: FlTitlesData(
+                    leftTitles: AxisTitles(
+                      sideTitles: SideTitles(
+                        showTitles: true,
+                        reservedSize: 40,
+                        getTitlesWidget: (value, meta) {
+                          if (value % 30 == 0) {
+                            return Text(value.toInt().toString(), style: const TextStyle(fontSize: 12));
+                          }
+                          return Container();
+                        },
+                      ),
+                    ),
+                    bottomTitles: AxisTitles(
+                      sideTitles: SideTitles(
+                        showTitles: true,
+                        reservedSize: 60,
+                        getTitlesWidget: (value, meta) {
+                          return Image.asset(
+                            _getMedalImage(value.toInt()),
+                            width: 50,
+                            height: 50,
+                          );
+                        },
+                      ),
+                    ),
+                    rightTitles: const AxisTitles(),
+                    topTitles: const AxisTitles(),
+                  ),
+                  barGroups: _buildBarGroups(medals, maxMedals),
+                  barTouchData: BarTouchData(
+                    enabled: true,
+                    handleBuiltInTouches: false,
+                    touchCallback: (event, response) {
+                      if (event.isInterestedForInteractions &&
+                          response != null &&
+                          response.spot != null) {
+                        setState(() {
+                          touchedGroupIndex = response.spot!.touchedBarGroupIndex;
+                        });
+                      } else {
+                        setState(() {
+                          touchedGroupIndex = -1;
+                        });
+                      }
+                    },
+                  ),
+                ),
               ),
             ),
           ],
@@ -180,40 +210,39 @@ class _RewardGraphState extends State<RewardGraph> {
     );
   }
 
-  Widget _buildGraphLine(String label) {
-    return Expanded(
-      child: Row(
-        children: [
-          const SizedBox(width: 4),
-          Text(label, style: const TextStyle(color: Color(0xFF666666), fontSize: 12)),
-          const SizedBox(width: 8),
-          const Expanded(child: Divider(color: Color(0xFFE0E0E0))),
-        ],
-      ),
-    );
+  List<BarChartGroupData> _buildBarGroups(Map<String, int> medals, int maxMedals) {
+    return [
+      _buildBarGroup(0, medals['bronze']!, Colors.brown, maxMedals),
+      _buildBarGroup(1, medals['silver']!, Colors.grey, maxMedals),
+      _buildBarGroup(2, medals['gold']!, Colors.amber, maxMedals),
+    ];
   }
 
-  Widget _buildMedalColumn(String imagePath, double barHeight, int count) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.end,
-      children: [
-        Container(
-          width: 50,
-          height: barHeight,
-          decoration: BoxDecoration(
-            color: const Color(0xFF6B9DA4),
-            borderRadius: BorderRadius.circular(8),
-          ),
-        ),
-        const SizedBox(height: 8),
-        Image.asset(imagePath, width: 55, height: 55),
-        const SizedBox(height: 4),
-        Text(
-          "$count",
-          style: const TextStyle(fontSize: 12, color: Colors.grey),
+  BarChartGroupData _buildBarGroup(int x, int value, Color color, int maxMedals) {
+    double barHeight = (value / maxMedals) * maxMedals.toDouble();
+    return BarChartGroupData(
+      x: x,
+      barRods: [
+        BarChartRodData(
+          toY: barHeight,
+          color: color,
+          width: 30,
+          borderRadius: BorderRadius.circular(8),
         ),
       ],
     );
   }
 
+  String _getMedalImage(int index) {
+    switch (index) {
+      case 0:
+        return 'assets/images/rewards/Bronze.png';
+      case 1:
+        return 'assets/images/rewards/Silver.png';
+      case 2:
+        return 'assets/images/rewards/Gold.png';
+      default:
+        return 'assets/images/rewards/Bronze.png';
+    }
+  }
 }
